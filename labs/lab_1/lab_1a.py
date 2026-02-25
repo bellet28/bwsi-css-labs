d6690a09691927b420bd71886dade5f437b09e2b
bellet28 <27tanb64@stu.smuhsd.org>
2026-02-25 10:32:06 -0800

    Update lab_1a.py
    Added a description of my activities and my hobbies!

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Belle T" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi! My name is Belle, and I am from California. In my free time, I enjoy playing the piano, going outside, and baking!I pursue my love for engineering through my school's FRC and Science Olympiad team! This year, my favorite event is Bungee. I'm excited to take these BWSI courses to build my knowledge and find new interests!")
 
 if __name__ == "__main__":
     main()
